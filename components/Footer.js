ca1603aab1495c057a21b77b1f4db1d0a747ee3e
Peter Paul Villarino <peter.villarino@cri-dc.litfirepublishing.com>
2021-02-22 09:34:04 +0800

    Page Done not responsive

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,30 @@
 const Footer = () => {
+    const getDate = () => {
+        const Today = new Date()
+        return Today.getFullYear()
+    }
+
     return (
         <footer>
-            <p>Copyright 2021 </p>
+            <div className="container flex">
+                <div className="text-cont">
+                    <h2>Peter</h2>
+                    <p>Life is good when you focus on brighter things</p>
+                    <div className="links">
+                        <a className="facebook" href="https://facebook.com" target="_blank"></a>
+                        <a className="instagram" href="https://instagram.com" target="_blank"></a>
+                        <a className="linkedin" href="https://linkedin.com" target="_blank"></a>
+                        <a className="github" href="https://github.com" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <div className="container flex">
+                    <p>
+                        © {getDate()} Copyright. Developed by Peter Paul Villarino
+                    </p>
+                </div>
+            </div>
         </footer>
     )
 }
