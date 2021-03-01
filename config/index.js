e411c1eb1c6f1b11e8c1aed859cf9a643031212b
Peter Paul Villarino <peterpaulvillarino@gmail.com>
2021-03-01 16:13:22 +0800

    Change URL

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://portfolio-5tjwanj0r-petrusmint.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://portfolio-p1jdlg34i-petrusmint.vercel.app'
+'
